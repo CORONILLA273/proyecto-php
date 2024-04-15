0dd2edc5b4ceca272ba94bcc027a2e41387e5f21
Coronilla <e.coronillahernandez@ugto.com>
2024-04-15 11:36:49 -0600

    Agregamos interface y metodo insertar en el servicio

---
+++ backend/services/UserService.php
@@ -0,0 +1,33 @@
+<?php
+    require_once '../models/User.php';
+    require_once '../db/Database.php';
+    require_once '../interfaces/UserInterface.php';
+
+    class UserService implements UserInterface {
+        private $db;
+
+        public function _constructor ($db) {
+            $this->db = $db;
+        }
+
+        public function registrarUsuario($usuario) {
+            $nombre = $usuario->getNombre();
+            $apaterno = $usuario->getApaterno();
+            $amaterno = $usuario->getAmaterno();
+            $direccion = $usuario->getDireccion();
+            $telefono = $usuario->getTelefono();
+            $correo = $usuario->getCorreo();
+            $username = $usuario->getUsuario();
+            $password = password_hash($usuario->getPassword(), PASSWORD_DEFAULT);
+
+            $sql_insertar = "INSERT INTO usuario (nombre, apaterno, amaterno, direccion, telefono, correo, username, password)
+            VALUES (null, '$nombre', '$apaterno', '$amaterno', '$direccion', '$telefono', '$correo', '$username', '$password')";
+
+            if ($this->db->query($sql_insertar) === TRUE){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+?>
